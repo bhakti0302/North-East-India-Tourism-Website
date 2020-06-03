2bf55665ba97685ee1de763e85223436308d626d
bhakti0302 <56402847+bhakti0302@users.noreply.github.com>
2020-06-03 14:35:37 +0530

    Add files via upload

---
+++ NET.php
@@ -0,0 +1,93 @@
+<html>
+
+<head>
+<title> North East India Tourism </title>
+<style>
+
+.header {
+  padding: 20px;
+  text-align: left;
+  background: white;
+  color: black;
+  font-size: 40px;
+ 
+}
+
+.container {
+  position: relative;
+  text-align: center;
+  color: white;
+  font-size: 70px;
+  font-family: Impact, Charcoal, sans-serif;
+  font-style: bold;
+}
+
+.top-left {
+  position: absolute;
+  top: 8px;
+  left: 16px;
+}
+
+.topnav {
+  overflow: hidden;
+  background-color: #FCF7F5;
+}
+
+.topnav a {
+  float: left;
+  font-style: bold;
+  
+  color: black;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 20px;
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.topnav a.active {
+  background-color: #56FEF3;
+  color: black;
+}
+
+</style>
+</head>
+<body>
+
+<div class="header">
+  <img align="left" src="logo.jpg" width="92" height="92"> <b>NORTH  EAST  INDIA  TOURISM</b>
+</div>
+
+<div class="container">
+<img src="back.JPG" height="90%" width="100%"/>
+<!--<div class="top-left">NORTH  EAST  INDIA  TOURISM</div> -->
+</div>
+
+<div class="topnav">
+  <a class="active" href="#home">Home</a>
+  <a href="sikkim.html">Sikkim</a>
+  <a href="arunachalPradesh.html">Arunachal Pradesh</a>
+  <a href="assam.html">Assam</a>
+  <a href="meghalaya.html">Meghalaya</a>
+ <a href="mizoram.html">Mizoram</a>
+ <a href="nagaland.html">Nagaland</a>
+ <a href="tripura.html">Tripura</a>
+</div>
+
+<div style="text-align:center;">
+  <h1>Want to explore North East India!</h1>
+  <h3>You are at the right place!</h3>
+</div>
+
+<div>
+<h4>Login here to get recent updates through email</h4>
+<button type="button"><a href="registrationform.html">Log In</a></button>
+</div>
+
+</body>
+
+</html>
